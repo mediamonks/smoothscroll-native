9d15b7a2a248e3566fcb4071eef874c839e3fd45
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2022-01-05 14:48:11 +0100

    add lerp story

--- packages/native-smooth-scroll/src/NativeSmoothScroll.stories.ts
+++ packages/native-smooth-scroll/src/NativeSmoothScroll.stories.ts
@@ -79,3 +79,47 @@ export const Sticky = (props: { sticky: boolean }) => {
 Sticky.args = {
   sticky: true,
 };
+
+export const Lerp = (props: { lerp: number }) => {
+  useEffect(() => {
+    const container = global.document.querySelector<HTMLElement>('#scroll-container');
+
+    let nativeSmoothScroll: NativeSmoothScroll;
+
+    if (container) {
+      nativeSmoothScroll = new NativeSmoothScroll();
+      nativeSmoothScroll.init(container, { lerp: props.lerp });
+
+      const scrollElements = Array.from(
+        global.document.querySelectorAll<HTMLElement>('.scroll-element'),
+      );
+
+      scrollElements.forEach((block) => {
+        nativeSmoothScroll.addElement(block);
+      });
+    }
+
+    return () => {
+      if (nativeSmoothScroll) {
+        nativeSmoothScroll.destruct();
+      }
+    };
+  });
+
+  return `<div id="scroll-container">
+      <div class="scroll-element scroll-element-1">1</div>
+      <div class="scroll-element scroll-element-2">2</div>
+      <div class="scroll-element scroll-element-3">3</div>
+      <div class="scroll-element scroll-element-4">4</div>
+    </div>`;
+};
+
+Lerp.args = {
+  lerp: 0.1,
+};
+
+Lerp.argTypes = {
+  lerp: {
+    control: { type: 'range', min: 0.01, max: 1, step: 0.01 },
+  },
+};
