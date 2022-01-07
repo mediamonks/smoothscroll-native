180d38d303d33037117caf4f265006018550e042
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2022-01-07 12:27:10 +0100

    add getRelativePosition, getAlignPosition, getAlignPositionByElement

--- packages/native-smooth-scroll/src/utils/positionUtils.ts
+++ packages/native-smooth-scroll/src/utils/positionUtils.ts
@@ -1,3 +1,5 @@
+import type { Align } from '../types';
+
 export function getStickyPosition(
   position: number,
   height: number,
@@ -14,9 +16,33 @@ export function getStickyPosition(
   return Math.max(position, 0);
 }
 
-export function getRelativePosition(element: HTMLElement, targetElement: HTMLElement) {
+export function getRelativePosition(
+  element: HTMLElement,
+  targetElement: HTMLElement,
+  align: Align = 'top',
+) {
   const elementBounds = element.getBoundingClientRect();
   const targetElementBounds = targetElement.getBoundingClientRect();
 
-  return elementBounds.top - targetElementBounds.top;
+  return (
+    elementBounds.top + getAlignPosition(0, elementBounds.height, align) - targetElementBounds.top
+  );
+}
+
+export function getAlignPosition(top: number, height: number, align: Align = 'top') {
+  switch (align) {
+    case 'middle':
+      return top + height * 0.5;
+    case 'bottom':
+      return top + height;
+    case 'top':
+    default:
+      return top;
+  }
+}
+
+export function getAlignPositionByElement(element: HTMLElement, align: Align = 'top') {
+  const bounds = element.getBoundingClientRect();
+
+  return getAlignPosition(bounds.top, bounds.height, align);
 }
