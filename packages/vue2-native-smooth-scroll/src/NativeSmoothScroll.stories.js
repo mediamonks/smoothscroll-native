f8e2b0a5ce6b40b8ac14ac3f8884cfe5b44ebdce
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2022-01-10 18:29:36 +0100

    add vue2 stories

--- packages/vue2-native-smooth-scroll/src/NativeSmoothScroll.stories.js
+++ packages/vue2-native-smooth-scroll/src/NativeSmoothScroll.stories.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line import/no-relative-packages
 import '../../native-smooth-scroll/src/styles.css';
-import { NativeSmoothScroll } from '@mediamonks/native-smooth-scroll/src';
+import { NativeSmoothScroll } from '@mediamonks/native-smooth-scroll';
+import ScrollContainer from './components/ScrollContainer/ScrollContainer.vue';
+import ScrollElement from './components/ScrollElement/ScrollElement.vue';
 
 export default {
   title: 'NativeSmoothScroll',
 };
 
-export const Default = (args, { argTypes }) => ({
+export const Simple = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: {},
   mounted() {
@@ -20,4 +22,15 @@ export const Default = (args, { argTypes }) => ({
   template: `<div ref="container" id="scroll-container">
     <div ref="element" v-for="(element, index) in Array(4)" :key="index" :class="['scroll-element', 'scroll-element-' + (index + 1)]">{{index + 1}}</div>
   </div>`,
+});
+
+export const Components = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { ScrollContainer, ScrollElement },
+  template: `<ScrollContainer :options="{ isResizeObserverEnabled: true }">
+    <ScrollElement class="scroll-element scroll-element-1">1</ScrollElement>
+    <ScrollElement class="scroll-element scroll-element-2">2</ScrollElement>
+    <ScrollElement class="scroll-element scroll-element-3">3</ScrollElement>
+    <ScrollElement class="scroll-element scroll-element-4">4</ScrollElement>
+  </ScrollContainer>`,
 });
---
+++ packages/vue2-native-smooth-scroll/src/index.js
@@ -0,0 +1,2 @@
+export * from './components/ScrollContainer/ScrollContainer.vue';
+export * from './components/ScrollElement/ScrollElement.vue';
